bd69be324aa5cda63e767c46e0851c11b3ab37df
= <=>
2021-12-03 14:57:53 +0100

    update agent.py

--- evry_project_strategy/nodes/agent.py
+++ evry_project_strategy/nodes/agent.py
@@ -53,32 +53,33 @@ class Robot:
         self.x = msg.pose.pose.position.x
         self.y = msg.pose.pose.position.y
 
-
-
-    def set_speed_angle(self,speed,angle):
-        """Method that retrieves the linear and angular velocities to send to publish
+    def constraint(self, val, min=-2.0, max=2.0):
+        """Method that limits the linear and angular velocities sent to the robot
 
         Args:
-            speed (float): desired linear velocity
-            angle (float): desired angular velocity
+            val (float): [Desired velocity to send
+            min (float, optional): Minimum velocity accepted. Defaults to -2.0.
+            max (float, optional): Maximum velocity accepted. Defaults to 2.0.
+
+        Returns:
+            float: Limited velocity whose value is within the range [min; max]
         """
-        self.speed = speed
-        self.angle = angle
-        self.pub_velocity()
+        #DO NOT TOUCH
+        if val < min: return min
+        if val > max: return max
+        return val
 
 
-    def pub_velocity(self):
-        """Method that publishes the proper linear and angular velocity commands on the related topic to move the robot
+    def set_speed_angle(self,linear,angular):
+        """Method that publishes the proper linear and angular velocities commands on the related topic to move the robot
+
+        Args:
+            linear (float): desired linear velocity
+            angular (float): desired angular velocity
         """
         cmd_vel = Twist()
-        cmd_vel.linear.x = self.speed
-        cmd_vel.linear.y = 0.0
-        cmd_vel.linear.z = 0.0
-
-        cmd_vel.angular.x = 0.0
-        cmd_vel.angular.y = 0.0
-        cmd_vel.angular.z = self.angle
-
+        cmd_vel.linear.x = self.constraint(linear)
+        cmd_vel.angular.z = self.constraint(angular, min=-1, max=-1)
         self.cmd_vel_pub.publish(cmd_vel)
 
 
@@ -96,7 +97,6 @@ class Robot:
             pose = Pose2D()
             pose.x = self.x
             pose.y = self.y
-            pose.theta = 0.0
             result = service(pose, int(self.robot_name[-1]))    #int(robot_name[-1]) corresponds to the id of the robot. It is also the id of the related flag
             return result.distance
         except rospy.ServiceException as e :
